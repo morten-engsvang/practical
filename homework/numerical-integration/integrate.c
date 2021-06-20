e634e32eeb9207c393b28a692300b77d142afea3
Morten <engsvang.morten@gmail.com>
2021-06-20 19:49:17 +0200

    del B færdig

--- homework/numerical-integration/integrate.c
+++ homework/numerical-integration/integrate.c
@@ -1,7 +1,7 @@
 #include<gsl/gsl_vector.h>
 #include<math.h>
 
-double open4point(double f(double), double a, double b, double delta, double eps, double f2, double f3){
+double open4point(double f(double), double a, double b, double delta, double eps, double f2, double f3,int numrecs){
 	/* Beregner rekursivt for 4 punkter i et åbent interval fra a til b */
 	double f1 = f(a+(1.0/6)*(b-a)), f4 = f(a+(5.0/6)*(b-a));
 	/* Jeg kan nu beregne mine approximative integraler */
@@ -9,19 +9,16 @@ double open4point(double f(double), double a, double b, double delta, double eps
 	double q = (f1+f2+f3+f4)*(b-a)/6; //Rektangel reglen
 	double tolerance = delta+eps*fabs(Q); //Den tilladte fejl
 	double error = fabs(Q-q);
-	/*
-	if(limit==0){
-		fprintf(stderr,"Recursion limit reached\n");
+	if(numrecs > 1000){
 		return Q;
 	}
-	*/
 	
 	if(error < tolerance){
 		return Q;
 	}
 	else {
-		double Q1 = open4point(f,a,(a+b)/2,delta/sqrt(2),eps,f1,f2);
-		double Q2 = open4point(f,(a+b)/2,b,delta/sqrt(2),eps,f1,f2);
+		double Q1 = open4point(f,a,(a+b)/2,delta/sqrt(2),eps,f1,f2,numrecs+1);
+		double Q2 = open4point(f,(a+b)/2,b,delta/sqrt(2),eps,f1,f2,numrecs+1);
 		return Q1+Q2;
 	}
 }
@@ -29,15 +26,16 @@ double open4point(double f(double), double a, double b, double delta, double eps
 double recursive_integrate(double f(double), double a, double b, double delta, double eps){
 	/* Vi får givet ende punkterne og kan beregne funktionsværdierne ved punkt 3 og 4 fra ligning 48, pga. måden open4point er sat op */
 	double f2 = f(a+(2.0/6)*(b-a)), f3 = f(a+(4.0/6)*(b-a));
+	int numrecs = 0;
 	/* Laver en rekursionsgrænse */
 	/* Starter rekursionen */
-	return open4point(f,a,b,delta,eps,f2,f3);
+	return open4point(f,a,b,delta,eps,f2,f3,numrecs);
 }
 
 double clenshaw_curtis(double f(double),double a,double b,double delta,double eps){
 	/* Clenshaw-Curtis variabel transformation af et generelt integral fra a til b */
 	double g(double y){
-		return f( (a+b)/2+(b-a)/2*cos(y) )*sin(y)*(b-a)/2;
+		return f( (a+b)/2+(b-a)/2*cos(y))*sin(y)*(b-a)/2;
 	}
 	return recursive_integrate(g,0,M_PI,delta,eps);
 }
--- homework/numerical-integration/main.c
+++ homework/numerical-integration/main.c
@@ -3,6 +3,7 @@
 #include<math.h>
 #include<stdlib.h>
 #include"integrate.h"
+#include <gsl/gsl_integration.h>
 
 double testf1(double x){
 	return sqrt(x);
@@ -18,19 +19,15 @@ double testf3(double x){
 	return U;
 }
 
-double testf4(double x){
-	double U = log(x)/sqrt(x);
-	return U;
-}
-
 
 
 int main(void){
-	double delta = 0.01;
-	double eps = 0.01;
+	double delta = 0.005;
+	double eps = 0.005;
 	int evals = 0;
-	double a = 0;
-	double b = 1;
+	double a = 0.0;
+	double b = 1.0;
+	printf("I have set the maximum allowed amount of recursions at 1000\n");
 	/* Jeg laver min tests til del A */
 	double function1(double x){
 		evals++;
@@ -48,12 +45,11 @@ int main(void){
 	}
 	double test2 = recursive_integrate(function2,a,b,delta,eps);
 	printf("The result of test 2: %g\n",test2);
+	printf("The error is: %g\n",fabs(test2-M_PI));
 	printf("The number of evaluations in test 2: %i\n",evals);
 	
 	/* Jeg laver mine tests til del b */
 	//første integral uden clenshaw
-	
-	
 	printf("Test 3: f(x) = 1/sqrt(x) from %g to %g without Clenshaw-Curtis\n",a,b);
 	evals = 0;
 	double function3(double x){
@@ -66,37 +62,32 @@ int main(void){
 	//første integral med clenshaw
 	printf("Test 4: f(x) = 1/sqrt(x) from %g to %g with Clenshaw-Curtis\n",a,b);
 	evals = 0;
-	
-	
-	/*
 	double test4 = clenshaw_curtis(function3,a,b,delta,eps);
 	printf("The result of test 4: %g\n",test4);
 	printf("The number of evaluations in test 4: %i\n",evals);
-	*/
-	
-	//andet integral uden clenshaw
-	
-	/*
-	printf("Test 5: f(x) = ln(x)/sqrt(x) from %g to %g without Clenshaw-Curtis\n",a,b);
+	//Jeg kan beregne pi igen men nu med Clenshaw-Curtis
+	printf("Test 5: repeat of test 2 but with Clenshaw-Curtis\n");
 	evals = 0;
-	double function4(double x){
-		evals++;
-		return testf4(x);
-	}
-	double test5 = recursive_integrate(function4,a,b,delta,eps);
+	double test5 = clenshaw_curtis(function2,a,b,delta,eps);
 	printf("The result of test 5: %g\n",test5);
-	printf("The number of evaluations in test 3: %i\n",evals);
-	*/
-	
-	//andet integral med clenshaw
-	
-	/*
-	printf("Test 6: f(x) = 1/sqrt(x) from %g to %g with Clenshaw-Curtis\n",a,b);
-	evals = 0;
-	double test6 = clenshaw_curtis(function4,a,b,delta,eps);
-	printf("The result of test 6: %g\n",test6);
-	printf("The number of evaluations in test 4: %i\n",evals);
-	*/
-	
+	printf("The error is: %g\n",fabs(test5-M_PI));
+	printf("The number of evaluations in test 5: %i\n",evals);
+	//Jeg kan så bruge GSL subroutinen:
+	printf("I can calculate the same using the QACS subsroutine from the GSL library\n");
+	int limits = 1e6; double result, error; evals = 0;
+	gsl_integration_workspace * workspace = gsl_integration_workspace_alloc(limits);
+	gsl_function F;
+	double function4(double x, void * params){
+		evals++;
+		return testf2(x);
+	}
+	F.function = &function4;
+	gsl_integration_qags(&F,a,b,delta,eps,limits,workspace,&result,&error);
+	printf("Result, error and number of function evaluations are: \n%.15g\n%.15g\n%i\n",result,fabs(result-M_PI),evals);
+	gsl_integration_workspace_free (workspace);
+	printf("-------------------------\n");
+	printf("A remarkable reduction in evaluations is observed for the integral 1/sqrt(x) from %g to %g\n",a,b);
+	printf("Using Clenshaw-Curtis to the pi integral gives a small reduction in error,\nbut also increases the amount of evaluations significantly.\n");
+	printf("The GSL routine beats me by a long shot both in error and function evaluations");
 return 0;
 }
