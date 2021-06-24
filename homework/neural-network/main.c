2e05cdd4a2aff389139970b64bc4e75f1c06e000
Morten <engsvang.morten@gmail.com>
2021-06-24 17:38:05 +0200

    start neural

---
+++ homework/neural-network/main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<gsl/gsl_vector.h>
+#include<gsl/gsl_matrix.h>
+#include<gsl/gsl_blas.h>
+#include<stdlib.h>
+#include<math.h>
+#define RND (double)rand()/RAND_MAX
+
+
+
+int main(void){
+	
+return 0;
+}
