c86b3ab56620b21b29541d19adf67374a9abb720
Morten <engsvang.morten@gmail.com>
2021-06-24 17:16:12 +0200

    del B færdig

--- homework/minimum/main.c
+++ homework/minimum/main.c
@@ -4,75 +4,10 @@
 #include<stdlib.h>
 #include<math.h>
 #include<stdio.h>
-#include"qr.h"
-#include"rkstep12.h"
 #include<float.h> //Giver machine epsilon som DBL_EPSILON
 double delta = sqrt(DBL_EPSILON);
 
 static int steplimit = 100000;
-
-void newton(void f(gsl_vector* x, gsl_vector* fx), gsl_vector* x, double eps) {
-	//I will construct the Jacobian using finite differences, delta_x = sqrt(DBL_EPSILON)
-	//And diagonalise it using the routine implemented in a previous homework.
-	int n=x->size; //Finder længden af x.
-	int maxsteps = 10000;
-	int step = 0;
-	gsl_matrix* J = gsl_matrix_alloc(n,n);
-	gsl_matrix* R = gsl_matrix_alloc(n,n);
-	gsl_vector* fx = gsl_vector_alloc(n);
-	gsl_vector* df = gsl_vector_alloc(n);
-	gsl_vector* newx = gsl_vector_alloc(n);
-	gsl_vector* backx = gsl_vector_alloc(n);
-	gsl_vector* backfx = gsl_vector_alloc(n);
-	
-	while (step < maxsteps){
-		f(x,fx); //Fylder fx med funktionsværdierne.
-		//Laver min Jacobian via. finite differences:
-		for (int j = 0; j < n; j++){
-			double xj = gsl_vector_get(x,j); //Gemmer elementets originalværdi til senere brug.
-			gsl_vector_set(x,j,xj+delta);
-			f(x,df);
-			gsl_vector_sub(df,fx); /* Giver df = f(x+delta)-f(x) for denne værdi af k=j i ligning 7, næste loop over i udvælger så alle ij elementer.*/
-			for (int i = 0; i < n; i++){
-				gsl_matrix_set(J,i,j,gsl_vector_get(df,i)/delta);
-			}
-			gsl_vector_set(x,j,xj); //Putter originalværdien tilbage
-		}
-		GS_decomp(J,R); //Foretager QR faktorisering
-		GS_solve(J,R,fx,newx); //Løser JDx=f(x) og placerer løsningen i newx.
-		gsl_vector_scale(newx,-1.0); //Løsningen til JDx=-f(x) er den negative af ovenstående løsning.
-		//Starter nu backtracking linesearch
-		double l = 1;
-		while (l > 1./64){ //Backtracker indtil l er 1/64 eller mindre.
-			gsl_vector_memcpy(backx,x); //Vi skal bruge x igen og GSL er destruktiv :(
-			gsl_vector_add(backx,newx); //laver x+dx vektoren ud fra den tidligere løsning, fuld skridt.
-			f(backx,backfx); //Finder funktionsværdien i f(x+dx), dvs. fuld skridt.
-			//Tjekker så om den opfylder ligning 8, gsl_blas_dnrm2 giver normen.
-			if ( gsl_blas_dnrm2(backfx) < (1-l/2)*gsl_blas_dnrm2(fx)){
-				break;
-			}
-			l *= 0.5; //Vi var gået for langt tilbage, så vi rykker frem.
-		}
-		gsl_vector_memcpy(backx,x); //Jeg laver kopi af x
-		gsl_vector_scale(newx,l); //Jeg skalerer min opdatering til x med den fundne l værdi
-		gsl_vector_add(backx,newx); // Lægger min opdatering til x
-		gsl_vector_memcpy(x,backx); // Erstatter x med det nye x.
-		f(x,fx); //Finder funktionsværdien i det nye punkt.
-		//Jeg kan nu tjekke om det har ændret sig nok, tjekker om ændringerne er store nok,
-		//eller om vi er tæt nok på til at f(x+dx) = 0
-		if ( gsl_blas_dnrm2(newx) < delta || gsl_blas_dnrm2(fx) < eps){
-			break;
-		}
-		step++; //Ej at forglemme
-	}
-	gsl_matrix_free(J);
-	gsl_matrix_free(R);
-	gsl_vector_free(fx);
-	gsl_vector_free(df);
-	gsl_vector_free(newx);
-	gsl_vector_free(backx);
-	gsl_vector_free(backfx);
-}
 
 void numeric_gradient(void F(gsl_vector* x,gsl_vector* fx), gsl_vector* x, gsl_vector* gradient){
 	//Sætter den givne vector gradient til gradienten i punktet givet ved vektor x for 
@@ -228,6 +163,63 @@ void himmelblau(gsl_vector* x, gsl_vector* fx){
 	gsl_vector_set(fx,0,value);
 }
 
+void higgs(gsl_vector* x, gsl_vector* fx){
+	int m = 30, n = 3;
+	double breitwigner;
+	double value = 0;
+	FILE * data = fopen("higgs.txt","r");
+	gsl_matrix* datamat =	gsl_matrix_alloc(m,n);
+	gsl_matrix_fscanf(data,datamat);
+	double mass = gsl_vector_get(x,0);
+	double gamma = gsl_vector_get(x,1);
+	double A = gsl_vector_get(x,2);
+	double E, sigma_i, dsigma_i;
+	for (int i = 0; i < m; i++) {
+		E = gsl_matrix_get(datamat,i,0);
+		sigma_i = gsl_matrix_get(datamat,i,1);
+		dsigma_i = gsl_matrix_get(datamat,i,2);
+		breitwigner = A/(pow(E-mass,2)+pow(gamma,2)/4); 
+		value += pow(breitwigner-sigma_i,2)/pow(dsigma_i,2); //Finder summen af error funktionen
+	}
+	gsl_vector_set(fx,0,value);
+	fclose(data);
+	gsl_matrix_free(datamat);
+}
+
+void plot_higgs(gsl_vector* x){
+	//Thanks to Kasper Larsen for setting this up :), I could have done it myself but was too lazy.
+	FILE * data = fopen("higgs.txt","r");
+	FILE * plotfile = fopen("plotdata.txt","w");
+	gsl_matrix * datmat = gsl_matrix_alloc(30,3);
+	gsl_matrix_fscanf(data,datmat);
+	
+	double mass = gsl_vector_get(x,0);
+	double Gamma = gsl_vector_get(x,1);
+	double A = gsl_vector_get(x,2);
+	double E, sigmai, dsigmai;
+	
+	fprintf(plotfile,"#index 0\n");
+	
+	for (int i = 0; i < 30; i++) {
+		E = gsl_matrix_get(datmat,i,0);
+		sigmai = gsl_matrix_get(datmat,i,1);
+		dsigmai = gsl_matrix_get(datmat,i,2);
+		fprintf(plotfile,"%10g %10g %10g\n",E,sigmai,dsigmai);
+	}
+	
+	fprintf(plotfile,"\n\n\n#index 1\n");
+	double Ein = gsl_matrix_get(datmat,0,0);
+	double Efin = gsl_matrix_get(datmat,29,0);
+	int numpoints = 300;
+	double h = (Efin - Ein) / (numpoints - 1.0);
+	for (int i = 0; i < numpoints; i++) {
+		fprintf(plotfile,"%10g %10g\n",Ein+h*i,A/(pow((Ein+h*i)-mass,2)+pow(Gamma,2)/4));
+	}
+	gsl_matrix_free(datmat);
+	fclose(plotfile);
+	fclose(data);
+}
+
 
 int main(void){
 	double eps = 0.00001;
@@ -256,10 +248,28 @@ int main(void){
 	printf("Starting from the point (%g,%g) it took %i steps.\n",gsl_vector_get(x1,0),gsl_vector_get(x1,1),steps);
 	printf("With an accuracy of %g and a maximum step limit of %i\n",eps,steplimit);
 	printf("--------------------------------------------\n\n");
+	//Minimerer så for Higgs
 	
-	
+	gsl_vector* higgs1 = gsl_vector_alloc(3);
+	gsl_vector* higgs2 = gsl_vector_alloc(3);
+	double mass = 125.3; 
+	double width = 2.03; 
+	double scale = 2;
+	gsl_vector_set(higgs1,0,mass);
+	gsl_vector_set(higgs1,1,width);
+	gsl_vector_set(higgs1,2,scale);
+	gsl_vector_memcpy(higgs2,higgs1);
+	steps = qnewton(higgs,higgs1,eps);
+	printf("Fitting the Breit-Wigner deviation function to the data gives a mass of %g, width of %g and scale-factor of %g\n",gsl_vector_get(higgs1,0),fabs(gsl_vector_get(higgs1,1)),gsl_vector_get(higgs1,2));
+	printf("Starting from a value of %g, %g and %g respectively.\n",gsl_vector_get(higgs2,0),gsl_vector_get(higgs2,1),gsl_vector_get(higgs2,2));
+	printf("From here it took %i steps with an accuracy of %g and a maximum step limit of %i\n",steps,eps,steplimit);
+	printf("It is however very finicky");
+	printf("--------------------------------------------\n\n");
+	plot_higgs(higgs1);
 	gsl_vector_free(x);
 	gsl_vector_free(x1);
+	gsl_vector_free(higgs1);
+	gsl_vector_free(higgs2);
 	gsl_vector_free(fx);
 return 0;
 }
